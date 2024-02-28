b783864a7b224efcbb6945c90740a98d41e5e3d3
wasude14 <97173701+wasude14@users.noreply.github.com>
2024-02-29 00:20:55 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,326 @@
+export const Bio = {
+  name: "Gaurav Raj",
+  roles: ["Software Developer", "App Developer", "Programmer", "Front End Developer","React Js"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/wasude14",
+  resume:
+    "https://drive.google.com/file/d/1HcaSwNrG7HChpE4fAsad4Vcbl8-8bFLp/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/gaurav-raj-461938234/",
+  twitter: "https://twitter.com/GauravR58153652",
+  insta: "https://www.instagram.com/raj_gaurav_28/",
+  facebook: "https://www.facebook.com/profile.php?id=100019180173800",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "XML",
+        image: "https://www.svgrepo.com/show/31053/xml.svg",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/181_Java_logo_logos-512.png",
+      },
+
+      {
+        name: "C++",
+        image:
+          "https://w7.pngwing.com/pngs/46/626/png-transparent-c-logo-the-c-programming-language-computer-icons-computer-programming-source-code-programming-miscellaneous-template-blue.png",
+      },
+
+      {
+        name: "Dart",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/7/7e/Dart-logo.png",
+      },
+      {
+        name: "C Language",
+        image:
+          "https://cdn.iconscout.com/icon/free/png-256/free-c-58-1175247.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1024px-Git_icon.svg.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0bEwhug_8AMQcJF4k9bR7LckXn9Jiz8ODGPgTmXOZ3Q&s",
+    role: "Flutter Developer",
+    company: "Software Development cell at Dev Sanskriti Vishwavidyalaya ",
+    date: "Oct 2022 - Nov 2022",
+    desc: "Built a university clone apllication project in one month internship program, where I worked on flutter with 4 team members.",
+    skills: [
+      "Flutter",
+      "Form Validation",
+      "Flutter Widgets",
+      "Firebase",
+      "Data Fetching",
+      "Carousel",
+      "Dart",
+      "Firabse Cloud",
+    ],
+    // doc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0bEwhug_8AMQcJF4k9bR7LckXn9Jiz8ODGPgTmXOZ3Q&s",
+  },
+];
+
+export const education = [
+  {
+    id: 1,
+    img: "https://lpu2017.files.wordpress.com/2015/08/unnamed.png",
+    school: "Lovely Professional University Phagwara, Punjab",
+    date: "Aug 2022 - Apr 2024",
+    grade: "8.23 CGPA",
+    desc: "I am currently pursuing a Master's degree in Computer Science at Lovely Professional University Phagwara, Punjab. I am in last  semesters and have a CGPA of 8.23. I have taken courses in Web Development, Algorithms, Object-Oriented Programming,Python,Tableau,Data Mining,Data Visualisation and Database Management Systems, among others.",
+    degree: "Master of Computer Application(MCA), Computer Science.",
+  },
+  {
+    id: 2,
+    img: "https://images.app.goo.gl/SYEEt8uGWHxtw4Ac6",
+    school: "L. N. Mishra Institute of Economic Development & Social Change, Patna.",
+    date: "Aug 2019 - Apr 2022",
+    grade: "7.39 CGPA",
+    desc: "I completed my Bachelor degree in Bachelor of Computer Application at LNMI of economic development & Social Change,Patna, where i stuied Computer Sciecne.",
+    degree: "Bachelor of Computer Application(BCA), Computer Science.",
+  },
+  {
+    id: 3,
+    img: "https://images.app.goo.gl/X5rB6KEpmTY9yDUF9",
+    school:
+      "School Of Creative Learning,Patna.",
+    date: "Apr 2018 - Apr 2019",
+    grade: "65%",
+    desc: "I completed my class 12th education at this School, where I studied Science with PCM",
+    degree: "CBSE(XII), Science with PCM",
+  },
+  {
+    id: 4,
+    img: "https://images.app.goo.gl/1FMwbGitwaz42c2r7",
+    school:
+      "Eklavya Eduacational Complex,Patna.",
+    date: "Apr 2017 - Apr 2018",
+    grade: "10 CGPA",
+    desc: "I completed my class 10th education at Eklavya Educational Complex,Patna",
+    degree: "CBSE BOard(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 7,
+    title: "Portfolio",
+    date: "Dec 2023 - Dec 2023",
+    description:
+      "As a dedicated and versatile professional, my portfolio showcases a diverse range of projects that highlight my skills such as Java, C++ etc. Leveraging a strong foundation in Flutter Development and reactJs Development, I have successfully delivered impactful solutions that demonstrate both creativity and technical proficiency.",
+    // image:
+    //   "https://camo.githubusercontent.com/fbf405040ae86b5d4a40f24e4ac034982cb8c0e2d850560ba64527997b258be8/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
+    tags: [
+      "JavaScript",
+      "JavaScript Libraries",
+      "HTML",
+      "Css",
+      "Data fetching through API's",
+    ],
+    category: "web app",
+    github: "https://github.com/sanjeevgangwar/Portfolio",
+    webapp: "https://sanjeevgangwar.github.io/Portfolio/",
+  },
+  {
+    id: 8,
+    title: "Todo-List",
+    date: "Jan 2024 - Jan 2024",
+    description:
+      "This is a web application that manages users' daily life tasks. Users can perform various operations such as deleting items, editing item names, and removing all items from the list. Local storage provides data stability; when you close the tab and browser, the data items will remain the same until you delete them.",
+    // image:
+    //   "https://camo.githubusercontent.com/fbf405040ae86b5d4a40f24e4ac034982cb8c0e2d850560ba64527997b258be8/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
+    tags: ["JavaScrip", "HTML", "Css", "JavaScript Libraries", "Local Storage"],
+    category: "web app",
+    github: "https://github.com/sanjeevgangwar/Todo-List",
+    webapp: "https://sanjeevgangwar.github.io/Todo-List/",
+  },
+
+  {
+    id: 1,
+    title: "Password Manager",
+    date: "Feb 2022 - Apr 2022",
+    description:
+      "This application is developed using Flutter. Essentially, it was created for personal use, serving as a tool to securely store passwords.",
+    // image:
+    //   "https://github.com/sanjeevgangwar/DSVV-Application/blob/main/assets/images/main_campus.jpg",
+    tags: ["Flutter", "Firebase", "Login Authention", "Dart", "Form"],
+    category: "android app",
+    github: "https://github.com/sanjeevgangwar/Password-Manager",
+    // webapp: "https://vexa-app.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Password Generator",
+    date: "Feb 2022 - Apr 2022",
+    description:
+      "I developed this application while learning Java. Consequently, the application is coded in Java and serves the purpose of generating random strong passwords based on the length of digits you specify. Additionally, I employed the Firebase authentication method to facilitate user login into the application.",
+    // image:
+    //   "https://github.com/sanjeevgangwar/DSVV-Application/blob/main/assets/images/main_campus.jpg",
+    tags: ["Java", "XML", "Login Authention"],
+    category: "android app",
+    // github: "https://github.com/sanjeevgangwar/Password-Manager",
+    // webapp: "https://vexa-app.netlify.app/",
+  },
+
+  {
+    id: 2,
+    title: "University Clone",
+    date: "Nov 2021 - Jan 2022",
+    description:
+      "I developed this Flutter application utilizing Firebase, Firebase Cloud, and data fetching from the internet through APIs. This project was completed within one month during my internship at the university, and I collaborated with two additional team members during its development.",
+    // image:
+    //   "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: [
+      "Flutter",
+      "Firebase",
+      "Firebase Cloud",
+      "Data Fetching",
+      "Form Validation",
+      "Flutter Widgets",
+      "Dart",
+    ],
+    category: "android app",
+    github: "https://github.com/sanjeevgangwar/DSVV-Application/tree/main",
+  },
+  {
+    id: 4,
+    title: "API Fetching ",
+    date: "Nov 2021 - Jan 2022",
+    description:
+      "I developed this Flutter application utilizing Flutter, widgets, and data fetching from the internet through APIs. This project simply fetchs data from API's.",
+
+    tags: ["Flutter", "Flutter Container", "Flutter Widgets", "Dart", "API's"],
+    category: "android app",
+    github: "https://github.com/sanjeevgangwar/API-Testing",
+    // webapp: "https://trackify.duckdns.org",
+  },
+
+  {
+    id: 6,
+    title: "Blood Bank Management System",
+    date: "Sept 2021 - Nov 2021",
+    description:
+      "Developed a comprehensive Blood Bank Management System as a part of a team project. The system was designed to efficiently manage and streamline blood donation processes. This web-based application incorporated user-friendly interfaces for both donors and administrators, ensuring a seamless experience.",
+    image:
+      "https://drive.google.com/file/d/1Ytxqgu_oYsWWWGz1KVH0Nk5pO_tLqAbv/view?usp=drive_link",
+    tags: ["Php", "HTML", "Css", "JavaScript"],
+    category: "web app",
+    github:
+      "https://github.com/sanjeevgangwar/Blood-Bank-Management-System-PHP-HTML",
+    webapp: "https://github.com/rishavchanda/Quiz-Earn",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2019, text: "Started my journey" },
+  { year: 2021, text: "Worked as a Full Stack Developer INtern in the Company North Bihar State Power Holding Pvt. Ltd." },
+  { year: 2022, text: "Completed Graduation " },
+  { year: 2022, text: "Started MCA in Punjab and Shared lot of Projects." },
+  { year: 2024, text: "Started my own platform" },
+];
